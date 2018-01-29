ba656842e7ef09ccefbf44b8c194a086b8e9c8cc
rushabh1191 <rushabhb@gsteckno.com>
2018-01-29 22:44:16 +0530

    Initial Commit

---
+++ FlochatTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlochatTest
+//
+//  Created by Rushabh Bhatt on 29/01/18.
+//  Copyright © 2018 Rushabh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
